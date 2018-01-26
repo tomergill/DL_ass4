59f7c083270d08421ca8679e0d0218faf969de6f
Tomer Gill <coolran103@gmail.com>
2018-01-26 12:44:09 +0200

    Success! Added printing to debug select cols...

--- GumbelSoftmaxTreeLSTM.py
+++ GumbelSoftmaxTreeLSTM.py
@@ -385,9 +385,6 @@ class GumbelSoftmaxTreeLSTM:
                     new_layer.append(parents)
 
                 y_st_before = y_st_before.npvalue()
-                print "y_st_before"
-                print y_st_before.shape
-                print y_st_before
                 y_st = np.eye(y_st_before.shape[1])[y_st_before.argmax()]  # one-hot Straight Through (ST) vector
 
                 # in forward pass, uses the one-hot y_st, but backwards propagates to the gumbel-softmax vector, y
