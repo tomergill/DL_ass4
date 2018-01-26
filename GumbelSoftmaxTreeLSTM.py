Worked! Now cumsum is wrong dimensions, so added debug printing

GumbelSoftmaxTreeLSTM.py
@@ -392,7 +392,12 @@ class GumbelSoftmaxTreeLSTM:
                 y_st = np.eye(y_st_before.shape[1])[y_st_before.argmax(axis=1)]  # one-hot Straight Through (ST) vector
 
                 # in forward pass, uses the one-hot y_st, but backwards propagates to the gumbel-softmax vector, y
-                print "y {} || y_st {}".format(y.dim(), y_st.shape)  #
+
+                print "########################################################################"
+                print "y.dim() {}".format(y.dim())
+                print "y_st.shape {}".format(y_st.shape)
+                print "########################################################################"
+
                 y_hat = dy.nobackprop(dy.inputTensor(y_st) - y) - y
 
                 Mt = layer[i].dim()[0][1]